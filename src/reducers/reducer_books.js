5891fe7e321810aa7becf9e8550ec178afea97be
imsambhav <imsambhav@gmail.com>
2018-05-13 00:26:17 -0700

    first

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,23 @@
+export default function() {
+  return [
+    {
+      title: 'JS1 JavaScript for Kids: A Playful Introduction to Programming',
+      pages: 101
+    },
+    {
+      title: 'JS2 Eloquent JavaScript: A Modern Introduction to Programming',
+      pages: 102
+    },
+    { title: 'JS3 JavaScript: The Good Parts', pages: 103 },
+    {
+      title:
+        'JS4 Programming JavaScript Applications: Robust Web Architecture with Node, HTML5, and Moderns JS Libraries',
+      pages: 104
+    },
+    {
+      title:
+        'JS5 Effective JavaScript: 68 Specific Ways to Harness the Power of JavaScript',
+      pages: 105
+    }
+  ];
+}
